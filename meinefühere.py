6a888c5e8aad6fdef23ccc337b585125689891e2
Bruh88 <60489319+Bruh88@users.noreply.github.com>
2020-02-07 22:49:04 +0200

    Update meinefühere.py

--- meinefühere.py
+++ meinefühere.py
@@ -1,6 +1,6 @@
 from er import *
 while True:
     print("Неззер пидорас")
-nezzer = input("Хто такой неззер для вас")
+nezzer = input("Хто такой неззер для вас?")
 if nezzer == "пидорас":
-    polzovatel_saita.ban(reason="оскорбление неззера")
+    polzovatel_saita.ban(reason="Ебать спасибо нахуй, вы правы, но банъ за оск. неззера")
