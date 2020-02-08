3cdc2e66b1488bc2ae9c1d42c98191c45e87a2a8
RockSlay <60819074+RockSlay@users.noreply.github.com>
2020-02-08 18:29:55 +0300

    Update meinefühere.py
    Код дополнен

--- meinefühere.py
+++ meinefühere.py
@@ -4,3 +4,5 @@ while True:
 nezzer = input("Хто такой неззер для вас?")
 if nezzer == "пидорас":
     polzovatel_saita.ban(reason="Ебать спасибо нахуй, вы правы, но банъ за оск. неззера")
+elif nezzer == "четкий поц":
+    polzovatel_saita.ban(reason="Ах ты, бан за похвалу неззера")
